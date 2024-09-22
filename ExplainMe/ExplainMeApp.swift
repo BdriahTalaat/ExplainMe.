7a3c83a8b9ef3ec10c5c7e390f6396fcf9264f4e
BdriahFakhrji <bdriahtalaat@gmail.com>
2024-09-22 09:46:46 +0300

    Initial Commit

---
+++ ExplainMe/ExplainMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplainMeApp.swift
+//  ExplainMe
+//
+//  Created by Bdriah Talaat on 19/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ExplainMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExplainMeTests/ExplainMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExplainMeTests.swift
+//  ExplainMeTests
+//
+//  Created by Bdriah Talaat on 19/03/1446 AH.
+//
+
+import Testing
+@testable import ExplainMe
+
+struct ExplainMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
